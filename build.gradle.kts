60b659defe45fa076cb6beb9a1ab2c39acbf6bd8
Tim Yates <tim.yates@gmail.com>
2022-07-19 15:41:16 +0100

    Update for new bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 plugins {
     id("groovy") 
     id("com.github.johnrengelman.shadow") version "7.1.2"
@@ -9,18 +11,22 @@ group = "com.example"
 
 repositories {
     mavenCentral()
+    maven { url = URI("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
 }
 
 dependencies {
+    annotationProcessor(platform("io.micronaut.serde:micronaut-serde-bom:1.1.1-SNAPSHOT"))
     annotationProcessor("io.micronaut:micronaut-http-validation")
     annotationProcessor("io.micronaut.serde:micronaut-serde-processor")
     implementation("io.micronaut:micronaut-http-client")
+    implementation(platform("io.micronaut.serde:micronaut-serde-bom:1.1.1-SNAPSHOT"))
+    implementation("io.micronaut:micronaut-management")
     implementation("io.micronaut:micronaut-runtime")
     implementation("io.micronaut.serde:micronaut-serde-jackson")
     implementation("jakarta.annotation:jakarta.annotation-api")
     runtimeOnly("ch.qos.logback:logback-classic")
     implementation("io.micronaut:micronaut-validation")
-
+    implementation("one.microstream:microstream-storage-restservice:07.00.00-MS-GA")
 }
 
 
--- src/main/java/com/example/MyController.java
+++ src/main/java/com/example/MyController.java
@@ -1,22 +1,56 @@
 package com.example;
 
-import io.micronaut.http.MediaType;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Produces;
+import io.micronaut.serde.annotation.SerdeImport;
+import one.microstream.storage.restadapter.types.ViewerObjectDescription;
 
 @Controller
+@SerdeImport(value = ViewerObjectDescription.class, mixin = MyController.ViewerObjectDescriptionMixin.class)
 public class MyController {
 
-    @Get("/flat")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Dto flat() {
-        return new Dto(new Object[]{"Hello", "World"});
+    private static final ViewerObjectDescription REF1 = createOneDescription("1000000000000000030", "1", "50c075d5-6116-4edb-9c2a-c91e3b84503d");
+    private static final ViewerObjectDescription REF2 = createOneDescription("1000000000000000032", "0", "Sergio");
+
+    @Get("/bad")
+    public ViewerObjectDescription bad() {
+        ViewerObjectDescription desc = createRootObject();
+        desc.setReferences(new ViewerObjectDescription[]{ REF1, REF2, null });
+        return desc;
     }
 
-    @Get("/nested")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Dto nested() {
-        return new Dto(new Object[]{"Hello", new Object[] { "World" }});
+    @Get("/good")
+    public ViewerObjectDescription good() {
+        ViewerObjectDescription desc = createRootObject();
+        desc.setReferences(new ViewerObjectDescription[]{ REF1, REF2 });
+        return desc;
+    }
+
+    private ViewerObjectDescription createRootObject() {
+        ViewerObjectDescription desc = new ViewerObjectDescription();
+        desc.setObjectId("1000000000000000031");
+        desc.setTypeId("1000069");
+        desc.setLength("3");
+        desc.setData(new Object[]{"1", "2", "3"});
+        return desc;
+    }
+
+    private static ViewerObjectDescription createOneDescription(String objectId, String variableLength, String data) {
+        ViewerObjectDescription ref1 = new ViewerObjectDescription();
+        ref1.setObjectId(objectId);
+        ref1.setTypeId("30");
+        ref1.setLength("1");
+        ref1.setVariableLength(new String[]{variableLength});
+        ref1.setSimplified(true);
+        ref1.setData(new Object[]{data});
+        return ref1;
+    }
+
+    // A mixin to keep showing null values and empty lists
+    @SuppressWarnings("DefaultAnnotationParam")
+    @JsonInclude(JsonInclude.Include.ALWAYS)
+    interface ViewerObjectDescriptionMixin {
     }
 }
